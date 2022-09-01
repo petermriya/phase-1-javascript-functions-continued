96675f9c9585317ec2df115605d8b4bfcf101f76
petermriya <petermuriya@gmail.com>
2022-09-01 11:52:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+const mondayWork = function (activity = "go to the office") {
+    return ("This Monday, I will ${activity}.");
+}
+const satWork = function (activity= "work from home"){
+    return ("Monday work (work from home)}.");
+} 
+function wrapAdjective (visual = '*'){
+    return function (adj = 'special'){
+        return "You are ${visual}$ {adj}${visual}!"
+    }
+}
